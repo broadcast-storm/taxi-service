f912e4cf230a14f5a91ef868ffc784e22e93a8e3
nikita220800 <nikita220800@mail.ru>
2021-07-10 16:22:17 +0300

    whitelist update

--- core/settings.py
+++ core/settings.py
@@ -82,6 +82,8 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:8080',
     'http://127.0.0.1:8080',
+    'http://localhost:8081',
+    'http://127.0.0.1:8081',
 )
 
 ROOT_URLCONF = 'core.urls'
